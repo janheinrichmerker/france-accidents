d340de3b6244ae89fd4b4ea321b4d17ff8cd7a63
Jan Heinrich Reimer <heinrich@reimer.family>
2021-07-28 17:40:25 +0200

    Fix format

--- src/Main.elm
+++ src/Main.elm
@@ -4,22 +4,35 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
+
+main : Program () Model Msg
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
 
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
 
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
