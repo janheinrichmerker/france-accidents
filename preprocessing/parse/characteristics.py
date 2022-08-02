8d1a17e5b404cca137e2c90fb5ceb41333c1f96d
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-08-02 18:34:32 +0200

    Move model to separate module

--- preprocessing/parse/characteristics.py
+++ preprocessing/parse/characteristics.py
@@ -1,74 +1,13 @@
 from csv import DictReader, DictWriter
 from datetime import datetime
-from enum import IntEnum
 from pathlib import Path
-from typing import NamedTuple, Iterable, Optional
+from typing import Iterable
 
 from tqdm.auto import tqdm
 
 from parse import Parser
-
-
-class Light(IntEnum):
-    DAYLIGHT = 1
-    DUSK_OR_DAWN = 2
-    NIGHT_WITHOUT_PUBLIC_LIGHTING = 3
-    NIGHT_WITH_PUBLIC_LIGHTING_OFF = 4
-    NIGHT_WITH_PUBLIC_LIGHTING_ON = 5
-
-
-class Intersection(IntEnum):
-    OUT_OF_INTERSECTION = 1
-    X_INTERSECTION = 2
-    T_INTERSECTION = 3
-    Y_INTERSECTION = 4
-    INTERSECTION_WITH_MORE_THAN_4_BRANCHES = 5
-    ROUNDABOUT = 6
-    PLACE = 7
-    LEVEL_CROSSING = 8
-    OTHER_INTERSECTION = 9
-
-
-class AtmosphericConditions(IntEnum):
-    NORMAL = 1
-    LIGHT_RAIN = 2
-    HEAVY_RAIN = 3
-    SNOW_HAIL = 4
-    FOG_SMOKE = 5
-    STRONG_WIND_STORM = 6
-    DAZZLING_WEATHER = 7
-    OVERCAST_WEATHER = 8
-    OTHER = 9
-
-
-class Collision(IntEnum):
-    TWO_VEHICLES_FRONT = 1
-    TWO_VEHICLES_FROM_THE_REAR = 2
-    TWO_VEHICLES_FROM_THE_SIDE = 3
-    THREE_OR_MORE_VEHICLES_IN_A_CHAIN = 4
-    THREE_OR_MORE_VEHICLES_MULTIPLE_COLLISIONS = 5
-    OTHER_COLLISION = 6
-    WITHOUT_COLLISION = 7
-
-
-class Location(IntEnum):
-    OUT_OF_TOWN = 1
-    IN_BUILT_UP_AREAS = 2
-
-
-class Characteristic(NamedTuple):
-    accident_id: int
-    timestamp: datetime
-    latitude: Optional[float]
-    longitude: Optional[float]
-    address: str
-    light: Optional[Light]
-    intersection: Optional[Intersection]
-    atmospheric_conditions: Optional[AtmosphericConditions]
-    collision: Optional[Collision]
-    location: Optional[Location]
-    department: str  # TODO Improve parsing.
-    commune: str  # TODO Improve parsing.
+from model import Characteristic, Light, Intersection, AtmosphericConditions, \
+    Collision, LocationRegime
 
 
 class CharacteristicsParser(Parser):
@@ -135,7 +74,7 @@ class CharacteristicsParser(Parser):
                         else None
                     ),
                     location=(
-                        Location(int(row["agg"]))
+                        LocationRegime(int(row["agg"]))
                         if True or row["agg"] != ""
                         else None
                     ),
