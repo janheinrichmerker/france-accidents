5316a1a9f2959985f392d9478c36ec607ecc31fc
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-08-03 18:15:06 +0200

    Improve file matching

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,14 +1,27 @@
 from pathlib import Path
 
-from cache import cache_artifacts
+from cache import cache_artifacts, DATA_DIR
+from model import Accident
+from parse.accident import AccidentsJsonlFormatter
 from parse.characteristics import CharacteristicsCsvParser
+from parse.locations import LocationsCsvParser
+from parse.vehicle import VehiclesCsvParser
+
+
+def _matches_file(file: Path, prefix: str) -> bool:
+    stem = file.stem
+    if not file.name.startswith(prefix):
+        return False
+    stem = stem.removeprefix(prefix)
+    stem = stem.removeprefix("-")
+    return stem.isnumeric()
 
 
 def _matching_files(files: list[Path], prefix: str) -> list[Path]:
     return [
         file
         for file in files
-        if file.name.startswith(prefix)
+        if _matches_file(file, prefix)
     ]
 
 
