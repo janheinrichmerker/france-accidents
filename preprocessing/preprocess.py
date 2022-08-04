fdcf38a590b185f002c36f9915d446c504ef51e4
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-08-04 04:19:21 +0200

    Implement preprocessing

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,10 +1,12 @@
+from collections import defaultdict
 from pathlib import Path
 
 from cache import cache_artifacts, DATA_DIR
-from model import Accident
+from model import Accident, Vehicle, Person, Characteristic, Location
 from parse.accident import AccidentsJsonlFormatter
 from parse.characteristics import CharacteristicsCsvParser
 from parse.locations import LocationsCsvParser
+from parse.person import PersonsCsvParser
 from parse.vehicle import VehiclesCsvParser
 
 
@@ -30,7 +32,44 @@ def main() -> None:
     characteristics = CharacteristicsCsvParser().parse(
         _matching_files(files, "caracteristiques")
     )
-    print(sum(1 for _ in characteristics))
+    locations = LocationsCsvParser().parse(_matching_files(files, "lieux"))
+    vehicles = VehiclesCsvParser().parse(_matching_files(files, "vehicules"))
+    persons = PersonsCsvParser().parse(_matching_files(files, "usagers"))
+    for _ in persons:
+        pass
+
+    accident_characteristics: dict[int, Characteristic] = {
+        characteristic.accident_id: characteristic
+        for characteristic in characteristics
+    }
+    accident_locations: dict[int, Location] = {
+        location.accident_id: location
+        for location in locations
+    }
+    accident_vehicles: dict[int, list[Vehicle]] = defaultdict(lambda: [])
+    for vehicle in vehicles:
+        accident_vehicles[vehicle.accident_id].append(vehicle)
+    accident_persons: dict[int, list[Person]] = defaultdict(lambda: [])
+    for person in persons:
+        accident_persons[person.accident_id].append(person)
+
+    accident_ids = (
+            set(accident_characteristics.keys()) |
+            set(accident_locations.keys()) |
+            set(accident_vehicles.keys()) |
+            set(accident_persons.keys())
+    )
+    accidents = [
+        Accident(
+            accident_id,
+            *accident_characteristics[accident_id][1:],
+            *accident_locations[accident_id][1:],
+            vehicles=accident_vehicles[accident_id],
+            persons=accident_persons[accident_id],
+        )
+        for accident_id in accident_ids
+    ]
+    AccidentsJsonlFormatter().format(accidents, DATA_DIR)
 
 
 if __name__ == "__main__":
