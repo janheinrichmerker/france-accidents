be970f1bb548a91f9c2aa211e7f38ebdb9a17517
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-08-04 04:18:37 +0200

    Add accident formatter

--- preprocessing/parse/accident.py
+++ preprocessing/parse/accident.py
@@ -2,49 +2,34 @@ from json import dumps
 from pathlib import Path
 from typing import Iterable
 
-from model import (
-    Characteristic, Accident
-)
+from tqdm.auto import tqdm
+
+from model import Accident
 from parse import Formatter
 
 
 class AccidentsJsonlFormatter(Formatter[Accident]):
     def format(
             self,
-            items: Iterable[Characteristic],
+            items: Iterable[Accident],
             output_dir: Path
     ) -> None:
         output_path = output_dir / "accidents.jsonl"
-
+        items = tqdm(
+            items,
+            desc="Formatting accidents",
+            unit="line",
+        )
         with output_path.open("w") as file:
             for item in items:
-                json = {
-                    "accident_id": item.accident_id,
-                    "timestamp": item.timestamp.isoformat(),
-                    "latitude": item.latitude or "",
-                    "longitude": item.longitude or "",
-                    "department": item.department,
-                    "commune": item.commune,
-                    "address": item.address,
-                    "location": (
-                        item.location.name
-                        if item.location else ""
-                    ),
-                    "light": (
-                        item.light.name
-                        if item.light else ""
-                    ),
-                    "atmospheric_conditions": (
-                        item.atmospheric_conditions.name
-                        if item.atmospheric_conditions else ""
-                    ),
-                    "intersection": (
-                        item.intersection.name
-                        if item.intersection else ""
-                    ),
-                    "collision": (
-                        item.collision.name
-                        if item.collision else ""
-                    ),
-                }
+                json = item._asdict()
+                json["timestamp"] = json["timestamp"].isoformat()
+                json["vehicles"] = [
+                    vehicle._asdict()
+                    for vehicle in json["vehicles"]
+                ]
+                json["persons"] = [
+                    person._asdict()
+                    for person in json["persons"]
+                ]
                 file.write(f"{dumps(json)}\n")
