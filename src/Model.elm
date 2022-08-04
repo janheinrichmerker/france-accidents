3c0c25e44444664af0083aacc9672a2144f1048e
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-08-04 18:36:11 +0200

    Update model

--- src/Model.elm
+++ src/Model.elm
@@ -1,297 +1,151 @@
 module Model exposing (..)
 
-import Dict exposing (Dict)
+import Date exposing (Date)
 import Set exposing (Set)
-import Time exposing (Posix)
 
 
-type alias Accident =
-    { id : AccidentId
-    , characteristic : Characteristic
-    , place : Place
-    , vehicles : Set Vehicle
-    }
-
-
-type alias Characteristics =
-    List ( AccidentId, Characteristic )
-
-
-type alias Characteristic =
-    { dateTime : Posix
-    , lighting : Lighting
-    , departement : Departement
-    , muncipality : Muncipality
-    , localisation : Localisation
-    , intersection : Intersection
-    , weather : Weather
-    , collision : Collision
-    , address : Address
-    , geoOriginator : GeoOriginator
-    , coordinates : Coordinates
-    }
-
-
-type alias AccidentId =
-    String
-
-
-type Lighting
-    = LightingFullDay
-    | LightingTwilightDawn
-    | LightingNightWithoutPublicLighting
-    | LightingNightWithPublicLightingNotLit
-    | LightingNightWithPublicLighting
-
-
-type alias Departement =
-    Int
-
-
-type alias Muncipality =
-    Int
-
-
-type Localisation
-    = LocalisationOutOfAgglomeration
-    | LocalisationInBuiltUpAreas
+type Light
+    = LightDaylight
+    | LightDuskOrDawn
+    | LightNightWithoutPublicLighting
+    | LightNightWithPublicLightingOff
+    | LightNightWithPublicLightingOn
 
 
 type Intersection
     = IntersectionOutOfIntersection
-    | IntersectionX
-    | IntersectionT
-    | IntersectionY
-    | IntersectionMoreThan4Branches
-    | IntersectionGiratory
+    | IntersectionXIntersection
+    | IntersectionTIntersection
+    | IntersectionYIntersection
+    | IntersectionIntersectionWithMoreThan4Branches
+    | IntersectionRoundabout
     | IntersectionPlace
     | IntersectionLevelCrossing
-    | IntersectionOther
+    | IntersectionOtherIntersection
 
 
-type Weather
-    = WeatherNormal
-    | WeatherLightRain
-    | WeatherHeavyRain
-    | WeatherSnowHail
-    | WeatherFogSmoke
-    | WeatherStrongWindStorm
-    | WeatherDazzling
-    | WeatherCloudy
-    | WeatherOther
+type AtmosphericConditions
+    = AtmosphericConditionsNormal
+    | AtmosphericConditionsLightRain
+    | AtmosphericConditionsHeavyRain
+    | AtmosphericConditionsSnowHail
+    | AtmosphericConditionsFogSmoke
+    | AtmosphericConditionsStrongWindStorm
+    | AtmosphericConditionsDazzlingWeather
+    | AtmosphericConditionsOvercastWeather
+    | AtmosphericConditionsOther
 
 
 type Collision
-    = CollisionTwoVehiclesFrontal
-    | CollisionTwoVehiclesRear
-    | CollisionTwoVehiclesSide
-    | CollisionThreeOrMoreVehiclesChain
+    = CollisionTwoVehiclesFront
+    | CollisionTwoVehiclesFromTheRear
+    | CollisionTwoVehiclesFromTheSide
+    | CollisionThreeOrMoreVehiclesInAChain
     | CollisionThreeOrMoreVehiclesMultipleCollisions
-    | CollisionOther
-    | CollisionNone
+    | CollisionOtherCollision
+    | CollisionWithoutCollision
 
 
-type alias Address =
-    String
+type LocationRegime
+    = LocationRegimeOutOfTown
+    | LocationRegimeInBuiltUpAreas
 
 
-type GeoOriginator
-    = GeoOriginatorMetropole
-    | GeoOriginatorAntilles
-    | GeoOriginatorGuyane
-    | GeoOriginatorReunion
-    | GeoOriginatorMayotte
-
-
-type alias Coordinates =
-    { latitude : Latitude
-    , longitude : Longitude
+type alias Characteristic =
+    { timestamp : Date
+    , latitude : Maybe Float
+    , longitude : Maybe Float
+    , address : String
+    , light : Maybe Light
+    , intersection : Maybe Intersection
+    , atmospheric_conditions : Maybe AtmosphericConditions
+    , collision : Maybe Collision
+    , location : Maybe LocationRegime
+    , department : String
+    , commune : String
     }
 
 
-type alias Latitude =
-    Float
-
-
-type alias Longitude =
-    Float
-
-
-type alias Places =
-    List ( AccidentId, Place )
-
-
-type alias Place =
-    { road : Road
-    , roadNumber : RoadNumber
-    , roadNumberIndex : RoadNumberIndex
-    , roadNumberIndexLetter : RoadNumberIndexLetter
-    , trafficRegime : TrafficRegime
-    , numberLanes : NumberLanes
-    , reservedLane : ReservedLane
-    , profile : Profile
-    , upstreamTerminalNumber : UpstreamTerminalNumber
-    , upstreamTerminalDistanceMeters : UpstreamTerminalDistanceMeters
-    , curvature : Curvature
-    , centralReservationWidth : CentralReservationWidth
-    , roadwayTrafficWidth : RoadwayTrafficWidth
-    , surfaceCondition : SurfaceCondition
-    , infrastructure : Infrastructure
-    , situation : Situation
-    , schoolPoint : SchoolPoint
-    }
-
-
-type Road
-    = RoadHighway
-    | RoadNationalRoad
-    | RoadDepartmentalRoad
-    | RoadCommunalWay
-    | RoadOffPublicNetwork
-    | RoadPublicParking
-    | RoadUrbanMetropolitan
-    | RoadOther
-
-
-type alias RoadNumber =
-    Int
-
-
-type alias RoadNumberIndex =
-    String
-
-
-type alias RoadNumberIndexLetter =
-    String
+type RoadCategory
+    = RoadCategoryHighway
+    | RoadCategoryNationalRoad
+    | RoadCategoryDepartmentalRoad
+    | RoadCategoryMunicipalRoads
+    | RoadCategoryOffThePublicNetwork
+    | RoadCategoryParkingLotOpenToPublicTraffic
+    | RoadCategoryUrbanMetropolitanRoads
+    | RoadCategoryOther
 
 
 type TrafficRegime
-    = TrafficRegimeUnknown
-    | TrafficRegimeOneWay
+    = TrafficRegimeOneWay
     | TrafficRegimeBidirectional
-    | TrafficRegimeSeparatedLanes
-    | TrafficRegimeVariableAssignmentLanes
+    | TrafficRegimeWithSeparateLanes
+    | TrafficRegimeWithVariableAssignmentLanes
 
 
-type alias NumberLanes =
-    Int
-
-
-type ReservedLane
-    = ReservedLaneNone
-    | ReservedLaneBikePath
-    | ReservedLaneCycleBank
-    | ReservedLaneReservedChannel
+type DedicatedLane
+    = DedicatedLaneNone
+    | DedicatedLaneBicyclePath
+    | DedicatedLaneCycleLane
+    | DedicatedLaneReservedLane
 
 
 type Profile
-    = ProfileUnknown
-    | ProfileFlat
-    | ProfileSloped
-    | ProfileHillTop
-    | ProfileHillBottom
-
-
-type alias UpstreamTerminalNumber =
-    Int
-
-
-type alias UpstreamTerminalDistanceMeters =
-    Int
+    = ProfileFlat
+    | ProfileSlope
+    | ProfileTopOfHill
+    | ProfileBottomOfHill
 
 
 type Curvature
     = CurvatureStraight
-    | CurvatureCurvedLeft
-    | CurvatureCurvedRight
-    | CurvatureCurvedS
+    | CurvatureLeftHandCurve
+    | CurvatureRightHandCurve
+    | CurvatureSCurve
 
 
-type alias CentralReservationWidth =
-    Int
-
-
-type alias RoadwayTrafficWidth =
-    Int
-
-
-type SurfaceCondition
-    = SurfaceConditionUnknown
-    | SurfaceConditionNormal
-    | SurfaceConditionWet
-    | SurfaceConditionPuddles
-    | SurfaceConditionFlooded
-    | SurfaceConditionSnow
-    | SurfaceConditionMud
-    | SurfaceConditionIcy
-    | SurfaceConditionFatOil
-    | SurfaceConditionOther
-
-
-type Infrastructure
-    = InfrastructureNone
-    | InfrastructureUndergroundOrTunnel
-    | InfrastructureBridgeOrOverpass
-    | InfrastructureExchangerOrConnectionBrace
-    | InfrastructureRailway
-    | InfrastructureCrossroads
-    | InfrastructurePedestrianArea
-    | InfrastructureTollZone
-
-
-type Situation
-    = SituationNone
-    | SituationOnRoad
-    | SituationOnHardShoulder
-    | SituationOnVerge
-    | SituationOnSidewalk
-    | SituationOnBikePath
-
-
-type alias SchoolPoint =
-    Int
-
-
-type alias Person =
-    { id : PersonId
-    , placeId : PlaceId
-    , personCategory : PersonType
-    , severity : Severity
-    , sex : Sex
-    , birthYear : BirthYear
-    , occasion : Occasion
-    , safetyEquipment : SafetyEquipment
-    , safetyEquipmentUsage : SafetyEquipmentUsage
-    , pedestrianLocation : PedestrianLocation
-    , action : Action
-    , accompaniment : Accompaniment
+type alias Location =
+    { road_category : RoadCategory
+    , road : String
+    , road_index_number : Maybe Int
+    , road_index_alpha : Maybe String
+    , traffic_regime : Maybe TrafficRegime
+    , lanes_count : Maybe Int
+    , dedicated_lane : Maybe DedicatedLane
+    , profile : Maybe Profile
+    , upstream_terminal : Maybe Int
+    , upstream_terminal_distance_meters : Maybe Float
+    , curvature : Maybe Curvature
+    , central_reservation_width_meters : Int
+    , road_traffic_width_meters : Float
     }
 
 
-type alias VehicleId =
-    Int
+type Place
+    = PlaceFrontLeftMotorcycleFront
+    | PlaceFrontRightMotorcycleRear
+    | PlaceRearRightMotorcycleSidecar
+    | PlaceRearLeft
+    | PlaceRearCenter
+    | PlaceFrontCenter
+    | PlaceCenterLeft
+    | PlaceCenterMiddle
+    | PlaceCenterRight
+    | PlacePedestrian
 
 
-type alias PersonId =
-    Int
-
-
-type alias PlaceId =
-    Int
-
-
-type PersonType
-    = PersonTypeDriver
-    | PersonTypePassenger
-    | PersonTypePedestrian
-    | PersonTypePedestrianRollerbladeScooter
+type PersonCategory
+    = PersonCategoryDriver
+    | PersonCategoryPassenger
+    | PersonCategoryPedestrian
 
 
 type Severity
-    = SeverityUnscathed
+    = SeverityUnharmed
     | SeverityKilled
-    | SeverityHospitalizedWounded
-    | SeverityLightInjury
+    | SeverityInjuredHospitalized
+    | SeveritySlightlyInjured
 
 
 type Sex
@@ -299,83 +153,233 @@ type Sex
     | SexMale
 
 
-type BirthYear
-    = Int
-
-
-type Occasion
-    = OccasionHomeWork
-    | OccasionHomeSchool
-    | OccasionShoppingShopping
-    | OccasionProfessional
-    | OccasionPromenadeLeisure
-    | OccasionOther
+type TravelReason
+    = TravelReasonHomeToWork
+    | TravelReasonHomeToSchool
+    | TravelReasonShopping
+    | TravelReasonProfessional
+    | TravelReasonWalkingLeisure
+    | TravelReasonOther
 
 
 type SafetyEquipment
     = SafetyEquipmentBelt
-    | SafetyEquipmentHelmet
-    | SafetyEquipmentChildrenSeat
-    | SafetyEquipmentReflectors
+    | SafetyEquipmentHeadset
+    | SafetyEquipmentChildrenDevice
+    | SafetyEquipmentReflectiveVest
+    | SafetyEquipmentAirbag
+    | SafetyEquipmentGloves
     | SafetyEquipmentOther
 
 
-type alias SafetyEquipmentUsage =
-    Bool
-
-
 type PedestrianLocation
-    = PedestrianLocationPavementFarFromPedestrianCrossing
-    | PedestrianLocationPavementNearPedestrianCrossing
-    | PedestrianLocationOnPedestrianCrossingWithoutSignals
-    | PedestrianLocationOnPedestrianCrossingWithSignals
+    = PedestrianLocationOnPavementAtLeast50MFromPedestrianCrossing
+    | PedestrianLocationOnPavementAtMost50MFromPedestrianCrossing
+    | PedestrianLocationOnPedestrianCrossingWithoutLightSignal
+    | PedestrianLocationOnPedestrianCrossingWithLightSignal
     | PedestrianLocationOnSidewalk
-    | PedestrianLocationOnVerge
-    | PedestrianLocationOnRefuge
-    | PedestrianLocationOnAgainstAisle
+    | PedestrianLocationOnRoadShoulder
+    | PedestrianLocationOnEmergencyBayOrShoulder
+    | PedestrianLocationOnCounterAisle
 
 
-type Action
-    = ActionMovingUnspecified
-    | ActionMovingBumpingVehicle
-    | ActionMovingOppositeDirection
-    | ActionCrossing
-    | ActionMasked
-    | ActionPlayingRunning
-    | ActionWithAnimal
-    | ActionOther
+type PedestrianAction
+    = PedestrianActionDirectionOfImpactingVehicle
+    | PedestrianActionOppositeDirectionOfImpactingVehicle
+    | PedestrianActionCrossing
+    | PedestrianActionMasked
+    | PedestrianActionPlayingRunning
+    | PedestrianActionWithAnimal
+    | PedestrianActionOther
+    | PedestrianActionGettingOnOffVehicle
 
 
-type Accompaniment
-    = AccompanimentAlone
-    | AccompanimentAccompanied
-    | AccompanimentInGroup
+type PedestrianCompany
+    = PedestrianCompanyAlone
+    | PedestrianCompanyAccompanied
+    | PedestrianCompanyGroup
 
 
-type alias Vehicle =
-    { persons : Dict PlaceId Person
-    , id : VehicleId
-    , flowDirection : FlowDirection
-    , vehicleType : VehicleType
+type alias Person =
+    { place : Maybe Place
+    , category : PersonCategory
+    , severity : Severity
+    , sex : Sex
+    , birth_year : Maybe Int
+    , travel_reason : Maybe TravelReason
+    , safety_equipment : Set SafetyEquipment
+    , pedestrian_location : Maybe PedestrianLocation
+    , pedestrian_action : Maybe PedestrianAction
+    , pedestrian_company : Maybe PedestrianCompany
     }
 
 
-type FlowDirection
-    = FlowDirectionIncreasingPostalAddressNumber
-    | FlowDirectionDescendingPostalAddressNumber
+type TrafficDirection
+    = TrafficDirectionIncreasingReferenceNumber
+    | TrafficDirectionDecreasingReferenceNumber
+    | TrafficDirectionNoReference
 
 
-type VehicleType
-    = VehicleTypeBicycle
-    | VehicleTypeMoped
-    | VehicleTypeCart
-    | VehicleTypeRegisteredScooter
-    | VehicleTypeMotorcycle
-    | VehicleTypeSideCar
-    | VehicleTypeVl
-    | VehicleTypeVlWithCaravan
-    | VehicleTypeVlWithTrailer
-    | VehicleTypeVu
-    | VehicleTypeVuWithCaravan
-    | VehicleTypeVuWithTrailer
-    | VehicleTypePl
+type VehicleCategory
+    = VehicleCategoryBicycle
+    | VehicleCategoryMopedLessThan50Cm3
+    | VehicleCategoryQuadWithBody
+    | VehicleCategoryRegisteredScooter
+    | VehicleCategoryMotorcycle
+    | VehicleCategorySidecar
+    | VehicleCategoryLightVehicleOnly
+    | VehicleCategoryLightVehicleWithCaravan
+    | VehicleCategoryLightVehicleWithTrailer
+    | VehicleCategoryCommercialVehicleOnlyWeightRatingAtLeast1_5TAtMost3_5T
+    | VehicleCategoryCommercialVehicleWithCaravanWeightRatingAtLeast1_5TAtMost3_5T
+    | VehicleCategoryCommercialVehicleWithTrailerWeightRatingAtLeast1_5TAtMost3_5T
+    | VehicleCategoryTruckOnlyWeightRatingMoreThan3_5TAtMost7_5T
+    | VehicleCategoryTruckOnlyWeightRatingMoreThan7_5T
+    | VehicleCategoryTruckMoreThan3_5TWithTrailer
+    | VehicleCategoryRoadTractorOnly
+    | VehicleCategoryRoadTractorWithSemiTrailer
+    | VehicleCategoryPublicTransport
+    | VehicleCategorySpecialMachine
+    | VehicleCategoryAgriculturalTractor
+    | VehicleCategoryScooterLessThan50Cm3
+    | VehicleCategoryMotorcycleMoreThan50Cm3AtMost125Cm3
+    | VehicleCategoryScooterMoreThan50Cm3AtMost125Cm3
+    | VehicleCategoryMotorcycleMoreThan125Cm3
+    | VehicleCategoryScooterMoreThan125Cm3
+    | VehicleCategoryQuadWithoutBodyAtMost50Cm3
+    | VehicleCategoryQuadWithoutBodyMoreThan50Cm3
+    | VehicleCategoryBuses
+    | VehicleCategoryBus
+    | VehicleCategoryTrain
+    | VehicleCategoryTramway
+    | VehicleCategoryTricycleAtMost50Cm3
+    | VehicleCategoryTricycleMoreThan50Cm3AtMost125Cm3
+    | VehicleCategoryTricycleMoreThan125Cm3
+    | VehicleCategoryPersonalTransporterMotorized
+    | VehicleCategoryPersonalTransporterUnmotorized
+    | VehicleCategoryPedelec
+    | VehicleCategoryOther
+
+
+type FixedObstacle
+    = FixedObstacleParkedVehicle
+    | FixedObstacleTree
+    | FixedObstacleMetalSlide
+    | FixedObstacleConcreteChute
+    | FixedObstacleOtherSlide
+    | FixedObstacleBuildingWallBridgePier
+    | FixedObstacleVerticalSignalSupportEmergencyCallStation
+    | FixedObstaclePost
+    | FixedObstacleStreetFurniture
+    | FixedObstacleParapet
+    | FixedObstacleIslandRefugeHighPost
+    | FixedObstacleSidewalkCurb
+    | FixedObstacleDitchEmbankmentRockFace
+    | FixedObstacleOtherOnRoad
+    | FixedObstacleOtherOnSidewalkOrShoulder
+    | FixedObstacleObstacleFreeRoadwayExit
+    | FixedObstacleNozzleAqueductHead
+
+
+type MobileObstacle
+    = MobileObstaclePedestrian
+    | MobileObstacleVehicle
+    | MobileObstacleRailVehicle
+    | MobileObstacleDomesticAnimal
+    | MobileObstacleWildAnimal
+    | MobileObstacleOther
+
+
+type ShockPoint
+    = ShockPointFront
+    | ShockPointFrontRight
+    | ShockPointFrontLeft
+    | ShockPointRear
+    | ShockPointRearRight
+    | ShockPointRearLeft
+    | ShockPointSideRight
+    | ShockPointSideLeft
+    | ShockPointMultipleImpactsRollovers
+
+
+type Manoeuvre
+    = ManoeuvreNoChangeOfDirection
+    | ManoeuvreSameDirectionSameLine
+    | ManoeuvreBetweenTwoLines
+    | ManoeuvreReversing
+    | ManoeuvreOppositeDirection
+    | ManoeuvreCrossingCentralReservation
+    | ManoeuvreOnBusLaneSameDirection
+    | ManoeuvreOnBusLaneOppositeDirection
+    | ManoeuvreMerging
+    | ManoeuvreTurningAroundOnRoad
+    | ManoeuvreChangingLanesLeft
+    | ManoeuvreChangingLanesRight
+    | ManoeuvreDeportedLeft
+    | ManoeuvreDeportedRight
+    | ManoeuvreTurningLeft
+    | ManoeuvreTurningRight
+    | ManoeuvreExceedingLeft
+    | ManoeuvreExceedingRight
+    | ManoeuvreCrossingRoadway
+    | ManoeuvreParkingManoeuvre
+    | ManoeuvreAvoidanceManoeuvre
+    | ManoeuvreDoorOpening
+    | ManoeuvreStoppedExcludingParking
+    | ManoeuvreParkedWithOccupants
+    | ManoeuvreDrivingOnSidewalk
+    | ManoeuvreOther
+
+
+type Engine
+    = EngineHydrocarbon
+    | EngineElectricHybrid
+    | EngineElectric
+    | EngineHydrogen
+    | EngineHuman
+    | EngineOther
+
+
+type alias Vehicle =
+    { vehicle_name : String
+    , vehicle_id : Maybe Int
+    , traffic_direction : Maybe TrafficDirection
+    , vehicle_category : VehicleCategory
+    , fixed_obstacle : Maybe FixedObstacle
+    , mobile_obstacle : Maybe MobileObstacle
+    , shock_point : Maybe ShockPoint
+    , primary_manoeuvre : Maybe Manoeuvre
+    , engine : Maybe Engine
+    , occupancy : Maybe Int
+    , persons : List Person
+    }
+
+
+type alias Accident =
+    { accident_id : Int
+    , timestamp : Date
+    , latitude : Maybe Float
+    , longitude : Maybe Float
+    , address : String
+    , light : Maybe Light
+    , intersection : Maybe Intersection
+    , atmospheric_conditions : Maybe AtmosphericConditions
+    , collision : Maybe Collision
+    , location : Maybe LocationRegime
+    , department : String
+    , commune : String
+    , road_category : RoadCategory
+    , road : String
+    , road_index_number : Maybe Int
+    , road_index_alpha : Maybe String
+    , traffic_regime : Maybe TrafficRegime
+    , lanes_count : Maybe Int
+    , dedicated_lane : Maybe DedicatedLane
+    , profile : Maybe Profile
+    , upstream_terminal : Maybe Int
+    , upstream_terminal_distance_meters : Maybe Float
+    , curvature : Maybe Curvature
+    , central_reservation_width_meters : Int
+    , road_traffic_width_meters : Float
+    , vehicles : List Vehicle
+    }
