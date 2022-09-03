397c78081ae344a141b444722813e49c40173e61
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-09-03 14:14:50 +0200

    Add visualization modules

---
+++ src/Visualization3.elm
@@ -0,0 +1,29 @@
+module Visualization3 exposing (..)
+
+import Html exposing (Html, text)
+import Model exposing (Accident)
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = NoOp
+
+
+init : Model
+init =
+    {}
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+
+view : Model -> List Accident -> Html msg
+view model accidents =
+    text (String.fromInt (List.length accidents))
