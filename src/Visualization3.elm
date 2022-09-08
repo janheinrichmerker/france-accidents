b041f6fdf7ebc51283e0d55c1395c1b869d86381
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-09-08 14:59:39 +0200

    Prepare tree vis model

--- src/Visualization3.elm
+++ src/Visualization3.elm
@@ -2,14 +2,36 @@ module Visualization3 exposing (..)
 
 import Html.Styled exposing (Html, br, div, text)
 import Model exposing (Accident)
+import Reorderable exposing (Reorderable, moveDown, moveUp)
+
+
+type TreeLayout
+    = TreeLayoutGraph
+    | TreeLayoutTreeMap
+
+
+type Dimension
+    = DimensionCollisionType
+    | DimensionRoadCategory
+    | DimensionLightCondition
+    | DimensionWeather
+    | DimensionIntersectionType
+    | DimensionRoadCurvature
+    | DimensionVehicleType
+    | DimensionSituation
 
 
 type alias Model =
-    {}
+    { treeLayout : TreeLayout
+    , dimensions : Reorderable Dimension
+    }
 
 
 type Msg
     = NoOp
+    | SelectTreeLayout TreeLayout
+    | MoveUpDimension Int
+    | MoveDownDimension Int
 
 
 label : String
@@ -19,7 +41,21 @@ label =
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { treeLayout = TreeLayoutTreeMap
+      , dimensions =
+            Reorderable.fromList
+                [ DimensionCollisionType
+                , DimensionRoadCategory
+                , DimensionLightCondition
+                , DimensionWeather
+                , DimensionIntersectionType
+                , DimensionRoadCurvature
+                , DimensionVehicleType
+                , DimensionSituation
+                ]
+      }
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -27,6 +63,15 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
+
+        SelectTreeLayout layout ->
+            ( { model | treeLayout = layout }, Cmd.none )
+
+        MoveUpDimension idx ->
+            ( { model | dimensions = moveUp idx model.dimensions }, Cmd.none )
+
+        MoveDownDimension idx ->
+            ( { model | dimensions = moveDown idx model.dimensions }, Cmd.none )
 
 
 view : Model -> List Accident -> Html Msg
