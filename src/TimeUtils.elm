b72d6231df90c7411c7168f2fff9016247de8c2c
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-09-09 12:06:24 +0200

    Add quarter and year time utils

--- src/TimeUtils.elm
+++ src/TimeUtils.elm
@@ -2,7 +2,53 @@ module TimeUtils exposing (..)
 
 import Time exposing (Posix)
 import Time.Date exposing (Weekday(..))
-import Time.DateTime exposing (DateTime, addDays, fromPosix, setDay, setHour, setMillisecond, setMinute, setSecond, setYear, toPosix, weekday)
+import Time.DateTime exposing (DateTime, addDays, fromPosix, month, setDay, setHour, setMillisecond, setMinute, setMonth, setSecond, setYear, toPosix, weekday)
+
+
+type Quarter
+    = FirstQuarter
+    | SecondQuarter
+    | ThirdQuarter
+    | FourthQuarter
+
+
+quarter : DateTime -> Quarter
+quarter date =
+    let
+        mon =
+            month date
+    in
+    if 1 <= mon && mon <= 3 then
+        FirstQuarter
+
+    else if 4 <= mon && mon <= 6 then
+        SecondQuarter
+
+    else if 7 <= mon && mon <= 9 then
+        ThirdQuarter
+
+    else
+        FourthQuarter
+
+
+setQuarterMonth : Int -> DateTime -> DateTime
+setQuarterMonth quarterMonth date =
+    let
+        offset =
+            case quarter date of
+                FirstQuarter ->
+                    0
+
+                SecondQuarter ->
+                    3
+
+                ThirdQuarter ->
+                    6
+
+                FourthQuarter ->
+                    9
+    in
+    setMonth (offset + quarterMonth) date
 
 
 setWeekday : Weekday -> DateTime -> DateTime
@@ -12,6 +58,46 @@ setWeekday newDay date =
 
     else
         date |> addDays -1 |> setWeekday newDay
+
+
+retainYear : Posix -> Posix
+retainYear time =
+    let
+        date : DateTime
+        date =
+            fromPosix time
+
+        floorDate : DateTime
+        floorDate =
+            date
+                |> setMonth 1
+                |> setDay 0
+                |> setHour 0
+                |> setMinute 0
+                |> setSecond 0
+                |> setMillisecond 0
+    in
+    toPosix floorDate
+
+
+retainQuarter : Posix -> Posix
+retainQuarter time =
+    let
+        date : DateTime
+        date =
+            fromPosix time
+
+        floorDate : DateTime
+        floorDate =
+            date
+                |> setQuarterMonth 1
+                |> setDay 0
+                |> setHour 0
+                |> setMinute 0
+                |> setSecond 0
+                |> setMillisecond 0
+    in
+    toPosix floorDate
 
 
 retainMonth : Posix -> Posix
