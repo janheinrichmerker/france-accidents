375308fd8e4405067cca7d72c04ece77d1b527b0
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-09-10 10:30:05 +0200

    Add accident coordinate functions

--- src/Visualization2.elm
+++ src/Visualization2.elm
@@ -1,12 +1,14 @@
 module Visualization2 exposing (..)
 
-import Html.Styled exposing (Html, br, div, text)
+import Axis
+import Color exposing (black)
+import Html.Styled exposing (Html, br, div, fromUnstyled, text)
 import Model exposing (Accident)
 import Scale exposing (ContinuousScale)
-import TypedSvg exposing (g, svg, text_)
+import TypedSvg exposing (circle, g, image, svg, text_)
 import TypedSvg.Attributes
 import TypedSvg.Core exposing (Svg)
-import TypedSvg.Types exposing (AnchorAlignment(..), Length(..), Transform(..))
+import TypedSvg.Types exposing (Align(..), AnchorAlignment(..), Length(..), MeetOrSlice(..), Opacity(..), Paint(..), Transform(..))
 import Utils exposing (tupleMean)
 
 
@@ -57,6 +59,18 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
+
+
+accidentsWithCoordinates : List Accident -> List ( Accident, Coordinates )
+accidentsWithCoordinates accidents =
+    accidents
+        |> List.filterMap accidentWithCoordinates
+
+
+accidentWithCoordinates : Accident -> Maybe ( Accident, Coordinates )
+accidentWithCoordinates accident =
+    Maybe.map2 Tuple.pair accident.latitude accident.longitude
+        |> Maybe.map (Tuple.pair accident)
 
 
 view : Model -> List Accident -> Html Msg
