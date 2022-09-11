7c422024a81de9eb828a16efb31fa03d00508af0
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-09-11 09:27:26 +0200

    Change to named partitions

--- src/Partition.elm
+++ src/Partition.elm
@@ -3,70 +3,91 @@ module Partition exposing (Filter, Partitioner, Partitioners, Partitions, equali
 import Tree exposing (Tree)
 
 
-type alias Filter a =
-    a -> Bool
+type alias Filter a comparable =
+    ( a -> Bool, comparable )
 
 
-type alias Partitioner a =
-    List (Filter a)
+type alias Partitioner a comparable =
+    List (Filter a comparable)
 
 
-type alias Partitioners a =
-    List (Partitioner a)
+type alias Partitioners a comparable =
+    List (Partitioner a comparable)
 
 
-type alias Partitions a =
-    List (List a)
+type alias Partition a comparable =
+    ( List a, comparable )
 
 
-equalityFilter : (a -> v) -> v -> Filter a
-equalityFilter mapper reference element =
-    mapper element == reference
+type alias Partitions a comparable =
+    List (Partition a comparable)
 
 
-maybeEqualityFilter : (a -> Maybe v) -> v -> Filter a
-maybeEqualityFilter mapper reference element =
-    case mapper element of
-        Nothing ->
-            False
-
-        Just x ->
-            x == reference
+type alias PartitionTree a comparable =
+    Tree (Partition a (List comparable))
 
 
-equalityPartitioner : (a -> v) -> List v -> Partitioner a
+equalityFilter : (a -> v) -> v -> comparable -> Filter a comparable
+equalityFilter mapper reference name =
+    ( \element -> mapper element == reference, name )
+
+
+maybeEqualityFilter : (a -> Maybe v) -> v -> comparable -> Filter a comparable
+maybeEqualityFilter mapper reference name =
+    ( \element ->
+        case mapper element of
+            Nothing ->
+                False
+
+            Just x ->
+                x == reference
+    , name
+    )
+
+
+equalityPartitioner : (a -> v) -> List ( v, comparable ) -> Partitioner a comparable
 equalityPartitioner mapper references =
-    List.map (equalityFilter mapper) references
+    List.map (\( ref, name ) -> equalityFilter mapper ref name) references
 
 
-maybeEqualityPartitioner : (a -> Maybe v) -> List v -> Partitioner a
+maybeEqualityPartitioner : (a -> Maybe v) -> List ( v, comparable ) -> Partitioner a comparable
 maybeEqualityPartitioner mapper references =
-    List.map (maybeEqualityFilter mapper) references
+    List.map (\( ref, name ) -> maybeEqualityFilter mapper ref name) references
 
 
-partition : List a -> Partitioner a -> Partitions a
+partition : List a -> Partitioner a comparable -> Partitions a comparable
 partition elements partitioner =
-    List.map (\filter -> List.filter filter elements) partitioner
+    List.map (\( filter, name ) -> ( List.filter filter elements, name )) partitioner
 
 
-partitionTree : List a -> Partitioners a -> Tree (List a)
-partitionTree elements partitioners =
+partitionTree : Partitioners a comparable -> List a -> PartitionTree a comparable
+partitionTree partitioners =
+    partitionTreeHelp partitioners []
+
+
+partitionTreeHelp : Partitioners a comparable -> List comparable -> List a -> PartitionTree a comparable
+partitionTreeHelp partitioners labels elements =
     case partitioners of
         [] ->
             -- No partitioner left, so we cannot partition the elements further.
-            Tree.singleton elements
+            Tree.singleton ( elements, labels )
 
         firstPartitioner :: remainingPartitioners ->
             -- Partition the elements across by the first partitioner
             -- and then build trees for each partition using the remaining partitioners.
             let
-                firstPartitions : Partitions a
+                firstPartitions : Partitions a comparable
                 firstPartitions =
                     partition elements firstPartitioner
+
+                mapPartition : Partition a comparable -> PartitionTree a comparable
+                mapPartition ( singePartition, singleLabel ) =
+                    partitionTreeHelp remainingPartitioners (labels ++ [ singleLabel ]) singePartition
+
+                mapPartitions : Partitions a comparable -> List (PartitionTree a comparable)
+                mapPartitions partitions =
+                    partitions |> List.map mapPartition
             in
             Tree.tree
-                elements
-                (List.map
-                    (\singePartition -> partitionTree singePartition remainingPartitioners)
-                    firstPartitions
-                )
+                ( elements, [] )
+                (mapPartitions firstPartitions)
